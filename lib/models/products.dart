a95f1b7d6d8f268acc80b997a2cfaf5a7a0bd2bf
astyrc <styrc.adam@gmail.com>
2021-12-20 15:01:21 +0100

    Fix product parsing

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -171,7 +171,7 @@ class WooProduct {
         priceHtml = json['price_html'],
         onSale = json['on_sale'],
         purchasable = json['purchasable'],
-        totalSales = json['total_sales'],
+        // totalSales = json['total_sales'],
         virtual = json['virtual'],
         downloadable = json['downloadable'],
         downloads = (json['downloads'] as List)
